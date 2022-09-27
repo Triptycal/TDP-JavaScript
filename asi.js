d3d4cd42f27c5fc91b506bc3c4296badd6979237
Triptycal <36767994+Triptycal@users.noreply.github.com>
2022-09-27 11:47:47 +0100

    Create asi.js

---
+++ asi.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// ASI
+// automatic
+// semi-colon
+// insertion
+
+// JavaScript automatically inserts semi-colons for us
+
+let a = 1;
+let b = 1;
+
+a
+++
+b
+
+console.log(`a = ${a}\nb = ${b}`);
+
+let c = 1;
+let d = 1;
+
+c++; d;
+
+console.log(`c = ${c}\nd = ${d}`);
